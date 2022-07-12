609615d1662d4b138c4690c345845acd5881d43d
justmaiklas <jan3lmj@gmail.com>
2022-07-12 10:46:09 +0300

    Fixed tests:
    	- Fixed Sort method
    	- Fixed IndexOf method :
    		Override Equals method in Product class.

---
+++ Module 3/source/Task1/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product : IEquatable<Product>
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; }
+
+        public double Price { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Product product)) return false;
+
+            return Equals(product);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+
+        public bool Equals(Product productToCompare)
+        {
+            if (productToCompare == null) return false;
+
+            return Name.Equals(productToCompare.Name) && Price.Equals(productToCompare.Price);
+        }
+    }
+}
